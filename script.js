ba41d5d377a8c656df8ea3a27fadcd1faeb7ed35
Daniel C <daniel@daniel.com>
2019-10-20 01:37:08 -0500

    2nd commit. working on conditionals and HTML appearing.

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function isBadName(array) {
    console.log("badName = (false)", badName);
    return badName;
 }
+
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
    form.addEventListener('submit',function() {
@@ -22,6 +23,8 @@ window.addEventListener("load", function() {
       let pilotNameArray = pilotName.value.split('');
       let copilotNameArray = copilotName.value.split('');
       let isBlank = false;
+      let div = document.getElementById("faultyItems"); //.style.visibility = "visible";
+      let heading2 = document.getElementById("launchStatus");
       if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
          isBlank = true;
          alert("All fields are required!");
@@ -35,17 +38,27 @@ window.addEventListener("load", function() {
             event.preventDefault();
          } 
       }
-      console.log(pilotNameArray);
-      console.log(copilotNameArray);
-      console.log("type of pilotName.value is ", typeof pilotName.value, "pilotName.Value is ",pilotName.value);
-      console.log("type of copilotName.value is ", typeof copilotName.value, "pilotName.Value is ",copilotName.value);
-      console.log("type of fuelLevel.value is ", typeof fuelLevel.value, typeof Number(fuelLevel), "fuelLevel.Value is ",fuelLevel.value);
-      console.log("type of cargoMass.value is ", typeof cargoMass.value, typeof Number(cargoMass), "cargoMass.Value is ",cargoMass.value);
-      
-   });
+      console.log("fuelLevel.value is type and value " , typeof fuelLevel.value, fuelLevel.value);
+      console.log("Number(fuelLevel.value) is type and value ", typeof Number(fuelLevel.value), Number(fuelLevel.value));
+      if (Number(fuelLevel.value) < 10000) {
+         console.log("hit low fuel level function");
+         document.getElementById("faultyItems").style.visibility = "visible";
+         div.innerHTML = `
+            <ol>
+               <li id="pilotStatus">Pilot ${pilotName.value} is Ready</li>
+               <li id="copilotStatus">Co-pilot ${copilotName.value} is Ready</li>
+               <li id="fuelStatus">Fuel level is too low for launch</li>
+               <li id="cargoStatus">Cargo mass low enough for launch</li>
+            </ol>
+         `; 
+         event.preventDefault();
+      }
+   }); 
 });
-
-
+//style="visibility:visible"
+// if (form.username.value == "") {
+//    document.getElementById("surnameMissing").style.visibility = "visible";        
+//  }
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
@@ -58,3 +71,10 @@ window.addEventListener("load", function() {
 </ol>
 <img src="${}">
 */
+   // console.log(pilotNameArray);
+   // console.log(copilotNameArray);
+   // console.log("type of pilotName.value is ", typeof pilotName.value, "pilotName.Value is ",pilotName.value);
+   // console.log("type of copilotName.value is ", typeof copilotName.value, "pilotName.Value is ",copilotName.value);
+   // console.log("type of fuelLevel.value is ", typeof fuelLevel.value, typeof Number(fuelLevel), "fuelLevel.Value is ",fuelLevel.value);
+   // console.log("type of cargoMass.value is ", typeof cargoMass.value, typeof Number(cargoMass), "cargoMass.Value is ",cargoMass.value);
+      
